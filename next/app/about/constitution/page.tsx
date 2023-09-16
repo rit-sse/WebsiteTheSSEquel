8aeebf07a44b07fe8cdbfbb3b1d1d70cdff3536c
Christian <cmmorgado53@gmail.com>
2023-09-16 16:52:30 -0400

    Inital page.tsx

--- next/app/about/constitution/page.tsx
+++ next/app/about/constitution/page.tsx
@@ -1,10 +1,24 @@
 // This file renders the /about/constitution route of the website.
-import RootLayout from "@/app/layout";
-
 export default function Constitution(){
     return (
-      <html lang="en">
-        
-      </html>
+      <>
+        <section className="text-slate-300">
+            <div className="mx-auto max-w-screen-xl px-4 py-4 lg:flex">
+                <div className="text-center flex flex-col items-center w-full max-w-3xl">
+                    <h1
+                    className="bg-gradient-to-r from-sky-400 to-emerald-400 bg-clip-text
+                                text-4xl/[3rem] font-extrabold text-transparent sm:text-5xl/[4rem]"
+                    >
+                        Our Constitution
+                    </h1>
+                    lornwpiehfuowbflwbfliwf wo fewgf hoe fgowf fg owhfow gfowgfowfeg ouwgfouwfe  o fgowuefgouwfgowef  
+                </div>
+
+            </div>
+            <div className="mx-auto max-w-screen-xl px-4 py-6 lg:flex">
+                lsjdbkcbwke
+            </div>
+        </section>
+      </>
     );
 }
