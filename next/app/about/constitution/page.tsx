ea39e10b30194385d52a087b70e6840cb894a3ec
Christian <cmmorgado53@gmail.com>
2023-09-16 15:42:17 -0400

    Basic layout added

--- next/app/about/constitution/page.tsx
+++ next/app/about/constitution/page.tsx
@@ -1,1 +1,10 @@
 // This file renders the /about/constitution route of the website.
+import RootLayout from "@/app/layout";
+
+export default function Constitution(){
+    return (
+      <html lang="en">
+        
+      </html>
+    );
+}
