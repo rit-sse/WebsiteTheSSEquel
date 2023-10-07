2fac3ffd10c150fdae17626f3f0ada22f2151094
Ryan <39078738+2nPlusOne@users.noreply.github.com>
2023-10-07 16:53:44 -0400

    Revert "Merge pull request #52 from rit-sse/dev/color-themes"
    This reverts commit c3465ba28c3b8152749d8d02e2a2bb41502e59df, reversing
    changes made to e012428018918a017f62a55115b215e4c891202e.

--- next/components/Footer.tsx
+++ next/components/Footer.tsx
@@ -1,12 +1,9 @@
-'use client'
-
 import ExternalHyperlinkText from "./common/ExternalHyperlinkText";
-import DarkModeToggle from "./common/darkModeToggle";
 
 const Footer: React.FC = () => {
     return (
         <footer className="flex items-center justify-between w-full h-auto p-3">
-            <DarkModeToggle />
+
         </footer>
     );
 };
--- next/components/nav/MobileNavDropdown.tsx
+++ next/components/nav/MobileNavDropdown.tsx
@@ -9,20 +9,20 @@ const MobileNavDropdown: React.FC<{ navItems: NavItemProps[] } & OnClickProps> =
   };
 
   return (
-    <ul className="mt-4 menu dropdown-content text-xl rounded-md w-64 shadow-lg shadow-base-200 bg-accent-focus">
+    <ul className="mt-4 dropdown-content menu text-xl rounded-md w-64 shadow-lg shadow-slate-950 bg-slate-800">
       {navItems.map((navItem, index) => (
         <li key={index}>
           {navItem.route ? (
-            <Link className="hover:bg-accent focus:bg-accent" href={navItem.route} onClick={(e) => handleNavigationClick(e)}>
+            <Link className="hover:bg-slate-700 focus:bg-slate-700" href={navItem.route} onClick={(e) => handleNavigationClick(e)}>
               <summary>{navItem.title}</summary>
             </Link>
           ) : (
-            <details>
-              <summary className="hover:bg-accent focus:bg-accent">{navItem.title}</summary>
+            <details open>
+              <summary className="hover:bg-slate-700 focus:bg-slate-700">{navItem.title}</summary>
               <ul>
                 {navItem.subItems?.map((subItem, subIndex) => (
                   <li key={subIndex}>
-                    <Link href={subItem.route || ""} className="hover:bg-accent focus:bg-accent" onClick={(e) => handleNavigationClick(e)}>
+                    <Link href={subItem.route || ""} className="hover:bg-slate-700 focus:bg-slate-700" onClick={(e) => handleNavigationClick(e)}>
                       <summary>{subItem.title}</summary>
                     </Link>
                   </li>
