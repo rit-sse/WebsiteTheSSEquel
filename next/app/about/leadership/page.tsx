30464b5cb0f7f09d7f547f811c9d3908796b457c
ntr0pie <tripathi.shreeshv@gmail.com>
2023-10-07 14:44:31 -0400

    Add LinkedIn SVG

--- next/app/about/leadership/page.tsx
+++ next/app/about/leadership/page.tsx
@@ -1,3 +1,5 @@
+import Github, { GitHub, LinkedIn } from '../../../components/common/Icons';
+
 export default function Leadership() {
 
     const team = [
@@ -8,7 +10,6 @@ export default function Leadership() {
             desc: "Lorem Ipsum is simply dummy text of the printing and typesettin industry.",
             linkedin: "javascript:void(0)",
             github: "javascript:void(0)",
-            portfolio: "javascript:void(0)"
         },
         {
             avatar: "https://api.uifaces.co/our-content/donated/xZ4wg2Xj.jpg",
@@ -17,7 +18,6 @@ export default function Leadership() {
             desc: "Lorem Ipsum is simply dummy text of the printing and typesettin industry.",
             linkedin: "javascript:void(0)",
             github: "javascript:void(0)",
-            portfolio: "javascript:void(0)"
         },
         {
             avatar: "https://randomuser.me/api/portraits/women/79.jpg",
@@ -26,7 +26,6 @@ export default function Leadership() {
             desc: "Lorem Ipsum is simply dummy text of the printing and typesettin industry.",
             linkedin: "javascript:void(0)",
             github: "javascript:void(0)",
-            portfolio: "javascript:void(0)"
         },
         {
             avatar: "https://randomuser.me/api/portraits/women/63.jpg",
@@ -35,7 +34,6 @@ export default function Leadership() {
             desc: "Lorem Ipsum is simply dummy text of the printing and typesettin industry.",
             linkedin: "javascript:void(0)",
             github: "javascript:void(0)",
-            portfolio: "javascript:void(0)"
         },
         {
             avatar: "https://randomuser.me/api/portraits/men/86.jpg",
@@ -44,7 +42,6 @@ export default function Leadership() {
             desc: "Lorem Ipsum is simply dummy text of the printing and typesettin industry.",
             linkedin: "javascript:void(0)",
             github: "javascript:void(0)",
-            portfolio: "javascript:void(0)"
         },
         {
             avatar: "https://randomuser.me/api/portraits/men/46.jpg",
@@ -53,7 +50,6 @@ export default function Leadership() {
             desc: "Lorem Ipsum is simply dummy text of the printing and typesettin industry.",
             linkedin: "javascript:void(0)",
             github: "javascript:void(0)",
-            portfolio: "javascript:void(0)"
         },
     ]
     
@@ -65,7 +61,7 @@ export default function Leadership() {
                     <h3 className="text-3xl font-semibold sm:text-4xl">
                         Meet our team
                     </h3>
-                    <p className="text-gray-600 mt-3">
+                    <p className="text-gray-400 mt-3">
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem, ipsum dolor sit amet consectetur adipisicing elit.  Lorem, ipsum dolor sit amet consectetur adipisicing elit.  Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
                     </p>
                 </div>
@@ -86,18 +82,11 @@ export default function Leadership() {
                                         <p className="text-indigo-300">{item.title}</p>
                                         <p className="text-gray-300 mt-2 px-10">{item.desc}</p>
                                         <div className="mt-4 flex justify-center gap-4 text-gray-400">
-                                            <a href={item.github}>
-                                                <svg className="w-5 h-5 duration-150 hover:text-gray-500" fill="currentColor" viewBox="0 0 48 48"><g clip-path="url(#clip0_17_80)"><path fill="currentColor" d="M15.1 43.5c18.11 0 28.017-15.006 28.017-28.016 0-.422-.01-.853-.029-1.275A19.998 19.998 0 0048 9.11c-1.795.798-3.7 1.32-5.652 1.546a9.9 9.9 0 004.33-5.445 19.794 19.794 0 01-6.251 2.39 9.86 9.86 0 00-16.788 8.979A27.97 27.97 0 013.346 6.299 9.859 9.859 0 006.393 19.44a9.86 9.86 0 01-4.462-1.228v.122a9.844 9.844 0 007.901 9.656 9.788 9.788 0 01-4.442.169 9.867 9.867 0 009.195 6.843A19.75 19.75 0 010 39.078 27.937 27.937 0 0015.1 43.5z" /></g><defs><clipPath id="clip0_17_80"><path fill="currentColor" d="M0 0h48v48H0z" /></clipPath></defs></svg>
-                                            </a> 
                                             <a href={item.linkedin}>
-                                                <svg className="w-5 h-5 duration-150 hover:text-gray-500" fill="none" viewBox="0 0 48 48">
-                                                    <g clip-path="url(#clip0_17_68)"><path fill="currentColor" d="M44.447 0H3.544C1.584 0 0 1.547 0 3.46V44.53C0 46.444 1.584 48 3.544 48h40.903C46.407 48 48 46.444 48 44.54V3.46C48 1.546 46.406 0 44.447 0zM14.24 40.903H7.116V17.991h7.125v22.912zM10.678 14.87a4.127 4.127 0 01-4.134-4.125 4.127 4.127 0 014.134-4.125 4.125 4.125 0 010 8.25zm30.225 26.034h-7.115V29.766c0-2.653-.047-6.075-3.704-6.075-3.703 0-4.265 2.896-4.265 5.887v11.325h-7.107V17.991h6.826v3.13h.093c.947-1.8 3.272-3.702 6.731-3.702 7.21 0 8.541 4.744 8.541 10.912v12.572z" />
-                                                    </g>
-                                                    <defs><clipPath id="clip0_17_68">
-                                                        <path fill="currentColor" d="M0 0h48v48H0z" />
-                                                            </clipPath>
-                                                        </defs>
-                                                </svg>
+                                                <LinkedIn/>
+                                            </a>
+                                            <a href={item.github}>
+                                            <GitHub/>
                                             </a>
                                         </div>
                                     </div>
--- next/components/common/Icons.tsx
+++ next/components/common/Icons.tsx
@@ -86,7 +86,7 @@ export const StarIcon = () => (
 
 export const GitHub = () => (
     <svg
-        className="w-6 h-6 fill-slate-400"
+        className="w-5 h-5 fill-slate-400"
         aria-hidden="true"
         viewBox="0 0 16 16"
         data-view-component="true"
@@ -95,3 +95,12 @@ export const GitHub = () => (
     </svg>
 );
 
+export const LinkedIn = () => (
+    <svg 
+        className="w-5 h-5 duration-150 hover:text-gray-500" 
+        fill="none" 
+        viewBox="0 0 48 48">
+            <g clip-path="url(#clip0_17_68)"><path fill="currentColor" d="M44.447 0H3.544C1.584 0 0 1.547 0 3.46V44.53C0 46.444 1.584 48 3.544 48h40.903C46.407 48 48 46.444 48 44.54V3.46C48 1.546 46.406 0 44.447 0zM14.24 40.903H7.116V17.991h7.125v22.912zM10.678 14.87a4.127 4.127 0 01-4.134-4.125 4.127 4.127 0 014.134-4.125 4.125 4.125 0 010 8.25zm30.225 26.034h-7.115V29.766c0-2.653-.047-6.075-3.704-6.075-3.703 0-4.265 2.896-4.265 5.887v11.325h-7.107V17.991h6.826v3.13h.093c.947-1.8 3.272-3.702 6.731-3.702 7.21 0 8.541 4.744 8.541 10.912v12.572z" /></g><defs><clipPath id="clip0_17_68">
+        <path fill="currentColor" d="M0 0h48v48H0z" /></clipPath></defs>
+    </svg>
+);
