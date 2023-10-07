7deaafe1ee70e163e91c6896844833e47996ad4b
Ryan <39078738+2nPlusOne@users.noreply.github.com>
2023-10-07 02:43:02 -0400

    Refactored colors to use semantic color theme

--- next/app/layout.tsx
+++ next/app/layout.tsx
@@ -22,9 +22,9 @@ export default function RootLayout({
 }) {
   return (
 
-    <html lang="en">
+    <html lang="en" data-theme="dark">
       <body
-        className={`min-h-screen flex flex-col text-slate-300 bg-gradient-to-b from-slate-900 to-slate-1000`}
+        className={`min-h-screen flex flex-col bg-gradient-to-b from-base-100 to-base-300`}
       >
         <Navbar />
         <main className="flex flex-col grow items-center p-2 md:p-4 lg:p-6 xl:p-8">
