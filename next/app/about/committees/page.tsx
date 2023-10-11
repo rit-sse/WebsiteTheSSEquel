03ca4ac2881f3a2f91f041e759b3c118034a31d5
saltyypanda <esh7943@rit.edu>
2023-10-11 16:01:19 -0400

    Made use of sm and md screen size query consistent

--- next/app/about/committees/page.tsx
+++ next/app/about/committees/page.tsx
@@ -5,25 +5,6 @@ import Image from 'next/image'
 // Initialization for ES Users
 import { Children } from 'react';
 
-// const ZCard: React.FC<{
-//   image: 'left' | 'right';
-//   children: React.ReactNode;
-// }> = ({ image, children }) => {
-//   const [left, right, ...rest] = Children.toArray(children);
-
-//   if (rest.length > 0) throw new Error("ZCard can only have two children");
-
-//   const classLeft = image == 'left' ? "w-1/3" : "w-2/3";
-//   const classRight = image == 'right' ? "w-1/3" : "w-2/3";
-
-//   return (
-//     <div className="flex flex-row sm:flex-col items-center gap-24">
-//       <div className={classLeft}>{left}</div>
-//       <div className={classRight}>{right}</div>
-//     </div>
-//   );
-// };
-
 const ZCard: React.FC<{
   image: 'left' | 'right';
   children: React.ReactNode;
@@ -32,9 +13,9 @@ const ZCard: React.FC<{
 
   if (rest.length > 0) throw new Error("ZCard can only have two children");
 
-  const classLeft = image === 'left' ? "w-full sm:w-1/3" : "w-full sm:w-2/3";
-  const classRight = image === 'right' ? "w-full sm:w-1/3" : "w-full sm:w-2/3";
-  const flexDirection = image === 'left' ? 'flex-col sm:flex-row' : 'flex-col-reverse sm:flex-row';
+  const classLeft = image === 'left' ? "w-full md:w-1/3" : "w-full md:w-2/3";
+  const classRight = image === 'right' ? "w-full md:w-1/3" : "w-full md:w-2/3";
+  const flexDirection = image === 'left' ? 'flex-col md:flex-row' : 'flex-col-reverse md:flex-row';
 
   return (
     <div className={`flex ${flexDirection} items-center gap-24 gap-y-0`}>
@@ -62,34 +43,34 @@ export default function Committees() {
     <>
       <section className="text-slate-200">
         <div className="flex flex-col items-center max-w-screen-xl">            
-            <div className="mx-auto px-4 sm: pt-16 md:py-16 max-w-2xl">
+            <div className="mx-auto px-4 sm: py-16 md:pb-8 max-w-2xl">
                 <div className="text-center flex flex-col items-center w-full">
                     <h1
-                    className="bg-gradient-to-r from-sky-400 to-emerald-400 bg-clip-text
-                                text-4xl/[3rem] font-extrabold text-transparent sm:text-5xl/[4rem]"
+                    className="bg-gradient-to-r from-primary to-secondary bg-clip-text
+                                text-4xl/[3rem] font-extrabold text-transparent md:text-5xl/[4rem]"
                     >
                     Committees
                     </h1>
 
-                    <p className="mx-auto mt-4 sm:text-xl/relaxed">
+                    <p className="mx-auto mt-4 text-xl/relaxed">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                         eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     </p>
                 </div>
             </div>
 
-            <div id='Committee Slots' className='pt-16 sm:pt-4'>
+            <div id='Committee Slots' className='pt-4'>
               <ZCard image='left'>
                 {placeholder_img}
                 <div className='text-left pt-4 pb-32 md:py-16'>
                   <h2
                   className="text-white bg-clip-text
-                              text-3xl/[3rem] font-bold text-transparent sm:text-1xl/[4rem]"
+                               font-bold text-transparent text-3xl/[3rem]"
                   >
                   Events
                   </h2>
 
-                  <p className="mt-4 pb-4 sm:text-xl/relaxed">
+                  <p className="mt-4 pb-4 text-xl/relaxed">
                   This is a description of the committee. Lorem ipsum dolor sit amet,
                   consectetur adipiscing elit. Integer ullamcorper dui eu ex laoreet,
                   sagittis aliquet mauris ornare. Nullam urna magna, hendrerit nec tortor
@@ -109,7 +90,7 @@ export default function Committees() {
                   Talks
                   </h2>
 
-                  <p className="mt-4 pb-4 sm:text-xl/relaxed">
+                  <p className="mt-4 pb-4 text-xl/relaxed">
                   This is a description of the committee. Lorem ipsum dolor sit amet,
                   consectetur adipiscing elit. Integer ullamcorper dui eu ex laoreet,
                   sagittis aliquet mauris ornare. Nullam urna magna, hendrerit nec tortor
@@ -126,12 +107,12 @@ export default function Committees() {
                 <div className='text-left pt-4 pb-32 md:py-16'>
                   <h2
                   className="text-white bg-clip-text
-                              text-3xl/[3rem] font-bold text-transparent sm:text-1xl/[4rem]"
+                               font-bold text-transparent text-3xl/[3rem]"
                   >
                   Projects
                   </h2>
 
-                  <p className="mt-4 pb-4 sm:text-xl/relaxed sm:pb-0">
+                  <p className="mt-4 pb-4 text-xl/relaxed sm:pb-0">
                   This is a description of the committee. Lorem ipsum dolor sit amet,
                   consectetur adipiscing elit. Integer ullamcorper dui eu ex laoreet,
                   sagittis aliquet mauris ornare. Nullam urna magna, hendrerit nec tortor
