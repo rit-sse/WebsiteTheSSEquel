6ca545579c47347005997941aed3ff08421b636a
saltyypanda <esh7943@rit.edu>
2023-10-14 17:29:16 -0400

    Imported and added calendar component to page

--- next/app/events/calendar/page.tsx
+++ next/app/events/calendar/page.tsx
@@ -1,6 +1,6 @@
-export default function Calendar() {
-    const calendarLink = "https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&showTitle=0&showPrint=0&src=ZXNoNzk0M0BnLnJpdC5lZHU&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=Y190MG5udTc4YTlkdHN0YjFjYTgwOTA2N2h2Y0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%2333B679&color=%230B8043&color=%233F51B5";
+import Calendar from "./Calendar";
 
+export default function EventsCalendar() {
     return (
         <>
             <div className="flex flex-col items-center w-screen h-screen max-w-screen-xl">            
@@ -15,12 +15,7 @@ export default function Calendar() {
                     </div>
                 </div>
 
-                <iframe
-                    src={calendarLink}
-                    className="border border-solid border-gray-700 w-full h-full"
-                    frameBorder="0"
-                    scrolling="no"
-                />
+                <Calendar/>
             </div>
         </>
     );
