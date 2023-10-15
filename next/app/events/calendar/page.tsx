cbe55198bc2ed67f6a84500cb2489ffceffe62e4
saltyypanda <esh7943@g.rit.edu>
2023-10-14 22:01:48 -0400

    Added metadata to calendar

--- next/app/events/calendar/page.tsx
+++ next/app/events/calendar/page.tsx
@@ -1,4 +1,11 @@
+import { Metadata } from "next";
 import Calendar from "./Calendar";
+
+export const metadata: Metadata = {
+    title: "Events Calendar",
+    description:
+      "The Society of Software Engineers hosts a wide variety of weekly events, ranging from talks and company visits to committee meetings and large seasonal parties."
+};
 
 export default function EventsCalendar() {
     return (
