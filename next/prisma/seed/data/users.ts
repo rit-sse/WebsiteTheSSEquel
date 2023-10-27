093c8a7e22cabf2f7e1efac530ffa5c6221f831c
jjb7866 <jjb7866@rit.edu>
2023-10-27 14:16:16 -0400

    created user.ts file and seed data code.

---
+++ next/prisma/seed/data/users.ts
@@ -0,0 +1,38 @@
+//The seed user data
+
+//User 1
+module.exports = [
+    //User 1
+    {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'johndoe@g.rit.edu',
+        isMember: true
+    },
+
+    //User 2
+    {
+        firstName: 'Jane',
+        lastName: 'Doe',
+        email: 'janedoe@g.rit.edu'
+    },
+
+    //User 3
+    {
+        firstName: 'John',
+        lastName: 'Smith',
+        email: 'johnsmith@g.rit.edu',
+        isMember: true,
+        emailVerified: true
+    },
+
+    //User 4
+    {
+        firstName: 'Jane',
+        lastName: 'Smith',
+        email: 'janesmith@g.rit.edu',
+        isMember: true,
+        profilePic: 'janepic.jpg',
+        emailVerified: true
+    }
+]
