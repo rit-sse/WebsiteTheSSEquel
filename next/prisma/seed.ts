791eafed1124863be56e0f9e93db7d88dfac3a02
jjb7866 <jjb7866@rit.edu>
2023-10-28 14:48:11 -0400

    Updated seed data

---
+++ next/prisma/seed.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+async function main() {
+    const johndoe = await prisma.user.upsert({
+        where: { email: 'johndoe@rit.edu' },
+        update: {},
+        create: {
+            firstName: 'John',
+            lastName: 'Doe',
+            email: 'johndoe@rit.edu'
+        },
+    })
+    console.log({ johndoe })
+}
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
