76d80c56dc7b168be2fb0b2a5a0adbfe329d2974
crazyforMcCafe <rayhmiot@gmail.com>
2023-10-28 16:51:00 -0400

    scaffolded props

--- next/app/go/page.tsx
+++ next/app/go/page.tsx
@@ -1,5 +1,21 @@
 import GoLink from "./GoLink";
 
+const goLinkData: { shortlink: string; url: string; description: string }[] = [
+  {
+    shortlink: "scoreboard",
+    url: "https://sse.rit.edu/go/scoreboard",
+    description: "The new membership scoreboard created with google sheets!",
+  }
+];
+
 export default function GoLinks() {
-  return <GoLink />;
+  const goLinkList = goLinkData.map(data => (
+    <GoLink
+      shortlink={data.shortlink}
+      url={data.url}
+      description={data.description}
+    />
+  ));
+
+  return <div>{goLinkList}</div>;
 }
