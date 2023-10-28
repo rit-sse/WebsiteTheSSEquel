8d18996ca2b0c2c9566b2c4eb8623cce6edbb927
PokeJofeJr4th <PokeJofeJr4th@gmail.com>
2023-10-28 16:36:06 -0400

    seed 2 mentors and 3 skills 😳

--- next/prisma/seed.ts
+++ next/prisma/seed.ts
@@ -25,10 +25,53 @@ async function seedOfficerPosition() { }
 async function seedOfficer() { }
 
 //Abigail
-async function seedMentor() { }
+async function seedMentor() {
+    const mentor_1 = await prisma.mentor.upsert({
+        where: { id: 1 },
+        update: {},
+        create: {
+            user_Id: 1,
+            expirationDate: new Date(2025, 4, 5),
+            isActive: true,
+        }
+    });
+    const mentor_2 = await prisma.mentor.upsert({
+        where: {id: 2},
+        update: {},
+        create: {
+            user_Id: 2,
+            expirationDate: new Date(2023, 10, 20),
+            isActive: false,
+        }
+    });
+    console.log({ mentor_1, mentor_2 });
+}
 
 //Abigail
-async function seedSkill() { }
+async function seedSkill() { 
+    const skill_ballin = await prisma.skill.upsert({
+        where: {id: 1},
+        update: {},
+        create: {
+            skill: "Ballin'"
+        }
+    });
+    const skill_sooubway = await prisma.skill.upsert({
+        where: {id: 2},
+        update: {},
+        create: {
+            skill: "Sandwich Artistry"
+        }
+    });
+    const skill_stealth = await prisma.skill.upsert({
+        where: {id: 3},
+        update: {},
+        create: {
+            skill: "Stealth"
+        }
+    })
+    console.log({ skill_ballin, skill_sooubway, skill_stealth });
+}
 
 //Garrett
 async function seedMentorSkill() { }
@@ -45,7 +88,8 @@ async function seedCourseTaken() { }
 
 async function main() {
     seedUser()
-
+    seedMentor()
+    seedSkill()
 }
 main()
     .then(async () => {
