1875779b116bf12f9c357038bf1b4a289379ec3c
Ashton Michelstein <aam1727@g.rit.edu>
2023-11-01 16:07:10 -0400

    fixed table in remark

--- next/lib/posts.ts
+++ next/lib/posts.ts
@@ -5,6 +5,7 @@ import rehypeStringify from 'rehype-stringify'
 import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
 import {unified} from 'unified'
+import remarkGfm from 'remark-gfm'
 
 const postsDirectory = path.join(process.cwd(), 'app', 'about', 'posts');
 export async function getPostData(url: string) {
@@ -15,6 +16,7 @@ export async function getPostData(url: string) {
     // const htmlContent = await remark().use(html).process(markdown);
     const htmlContent = await unified()
     .use(remarkParse)
+    .use(remarkGfm)
     .use(remarkRehype)
     .use(rehypeSanitize)
     .use(rehypeStringify)
