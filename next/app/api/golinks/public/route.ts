925c9f4e8a5d9c7854372c0a278f13d469ec70f9
jjb7866 <jjb7866@rit.edu>
2023-11-04 16:55:41 -0400

    Get methods for golinks active

--- next/app/api/golinks/public/route.ts
+++ next/app/api/golinks/public/route.ts
@@ -3,6 +3,6 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
 export async function GET() {
-    const goLinks = await prisma.goLinks.findMany({ where: { isPublic: false } })
-    return goLinks
+    const goLinks = await prisma.goLinks.findMany({ where: { isPublic: true } })
+    return Response.json(goLinks)
 }
