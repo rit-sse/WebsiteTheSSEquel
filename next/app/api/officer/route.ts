99d4b09fb8da4b201eab5758cbe0f76d90c45e32
Anderson Cardenas <arc8809@rit.edu.com>
2023-11-04 16:56:51 -0400

    officer route

---
+++ next/app/api/officer/route.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+export async function GET() {
+    const officer = await prisma.officer.findMany({ where: {is_active:true} })
+    return officer
+}
