25f4673bf1fcd6daf7226742afb7912504e97aec
PokeJofeJr4th <PokeJofeJr4th@gmail.com>
2023-11-04 16:56:42 -0400

    full CRUD the departments

--- next/app/api/departments/route.ts
+++ next/app/api/departments/route.ts
@@ -2,10 +2,11 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function get_departments() {
+export async function GET() {
   const allDepts = await prisma.department.findMany({
     select: {
       title: true,
+      shortTitle: true,
       course: {
         select: {
           title: true,
@@ -14,37 +15,41 @@ async function get_departments() {
       },
     },
   });
-  return allDepts;
-}
-
-// async function make_department(title: string) {
-//   await prisma.department.create({
-//     data: {
-//       title,
-//     },
-//   });
-// }
-
-// async function remove_department(id: number) {
-//   await prisma.department.delete({ where: { id } });
-// }
-
-export async function GET() {
-  const allDepts = await get_departments();
   console.log("All Departments:", allDepts);
   return Response.json(allDepts);
 }
 
-// export async function POST(request: Request) {
-//   const body = await request.json();
-//   const title = body.title;
-//   await make_department(title);
-//   return new Response(`Created new department: ${title}`, { status: 200 });
-// }
+export async function POST(request: Request) {
+  const body = await request.json();
+  // TODO: Validate the titles
+  const title = body.title;
+  const shortTitle = body.shortTitle;
+  await prisma.department.create({
+    data: {
+      title,
+      shortTitle,
+    },
+  });
+  return new Response(`Created new department: ${title}`, { status: 200 });
+}
 
-// export async function DELETE(request: Request) {
-//   const body = await request.json();
-//   const id = body.id;
-//   await remove_department(id);
-//   return new Response("Deleted department", { status: 200 });
-// }
+export async function DELETE(request: Request) {
+  const body = await request.json();
+  const id = body.id;
+  // TODO: Validate the id
+  await prisma.department.delete({ where: { id } });
+  return new Response("Deleted department", { status: 200 });
+}
+
+export async function PUT(request: Request) {
+  const body = await request.json();
+  // TODO: Validate input
+  const id = body.id;
+  const title = body.title;
+  const shortTitle = body.shortTitle;
+  await prisma.department.update({
+    where: { id },
+    data: { title, shortTitle },
+  });
+  return new Response("Updated department", { status: 200 });
+}
