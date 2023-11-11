166bf2f8487092cece5681f8eb486ce17bc698bc
saltyypanda <esh7943@rit.edu>
2023-11-11 16:17:03 -0500

    Modified ZCardContent to have alt function, created implementation of ZCardContent

--- next/app/about/committees/CommitteeSlot.tsx
+++ next/app/about/committees/CommitteeSlot.tsx
@@ -1,30 +1,47 @@
-/*  Creates a div for each committee storing the committee name and description
-    Params:
-      name(string): the name of the committee
-      description(string): a description of the committee
-      textSide(string): if the text is on the left or the right of the image
-*/
-const CommitteeSlot: React.FC<{
-    name: string;
-    description: string;
-  }> = ({ name, description }) => {
-    const generalClasses = 'text-left pt-4 pb-32 md:py-16'
-    const nameClasses = 'bg-clip-text font-bold text-3xl/[3rem]'
-    const descriptionClasses = 'mt-4 pb-4 text-xl/relaxed'
-  
-    return (
-      <div className={generalClasses}>
-        <h2
-          className={nameClasses}
-        >
-          {name}
-        </h2>
-  
-        <p className={descriptionClasses}>
-          {description}
-        </p>
-      </div>
-    );
-  };
+import { ZCardContent } from "@/types/ZCardContent";
+import { FC } from "react";
 
-export default CommitteeSlot;
+export class CommitteeSlot implements ZCardContent {
+    imageSrc: string;
+    title: string;
+    body: string;
+
+    constructor(
+        private imageSrcString: string,
+        private titleName: string,
+        private bodyContent: string
+    ) {
+        this.imageSrc = imageSrcString;
+        this.title = titleName;
+        this.body = bodyContent;
+    }
+
+    getAlt(): string {
+        return "Image of" + this.title;
+    }
+
+    toContent(): FC<{}> {
+        return () => {
+            const generalClasses = 'text-left pt-4 pb-32 md:py-16'
+            const nameClasses = 'bg-clip-text font-bold text-3xl/[3rem]'
+            const descriptionClasses = 'mt-4 pb-4 text-xl/relaxed'
+        
+            return (
+            <div className={generalClasses}>
+                <h2 className={nameClasses}>
+                    {this.title}
+                </h2>
+        
+                <p className={descriptionClasses}>
+                    {this.body}
+                </p>
+            </div>
+            );
+        }
+    }
+
+    getImageSrc(): string {
+        return this.imageSrc;
+    }
+
+}
