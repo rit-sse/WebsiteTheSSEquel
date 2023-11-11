534ca1b0f2f84bd6f232522e9ee739f976aacb29
saltyypanda <esh7943@rit.edu>
2023-11-11 16:18:43 -0500

    Added ZCardContainer to hold ZCards

---
+++ next/components/ZCardContainer.tsx
@@ -0,0 +1,25 @@
+import { Children } from 'react';
+import Image from 'next/image';
+import { ZCardContent } from '@/types/ZCardContent';
+import ZCard from './ZCard';
+
+const ZCardContainer: React.FC<{
+    contentSlots: ZCardContent[];
+  }> = ({ contentSlots }) => {
+    return (
+        <div className='pt-4'>
+            {contentSlots.map((slot, index) => (
+                <ZCard imageSide={(index % 2) === 0 ? 'left' : 'right'}>
+                    <Image
+                        src={slot.getImageSrc()}
+                        alt={slot.getAlt()}
+                        className="w-full h-auto rounded-md"
+                    />
+                    {slot.toContent()({})}
+                </ZCard>
+            ))}
+        </div>
+      );
+  };
+  
+export default ZCardContainer;
