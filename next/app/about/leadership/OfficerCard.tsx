60562df93badf1abee50a4330dae5d9230cb84ed
Ryan <39078738+2nPlusOne@users.noreply.github.com>
2023-11-12 14:02:04 -0500

    Revised typescript usage

--- next/app/about/leadership/OfficerCard.tsx
+++ next/app/about/leadership/OfficerCard.tsx
@@ -1,37 +1,30 @@
 import { GitHub, LinkedIn, Email } from "../../../components/common/Icons";
+import { TeamMember } from "./team";
 
 interface OfficerCardProps {
-  item: {
-    avatar: string;
-    name: string;
-    title: string;
-    desc: string;
-    linkedin: string;
-    github: string;
-    email: string;
-  };
+  teamMember: TeamMember;
 }
 
-export default function OfficerCard({ item }: OfficerCardProps) {
+export default function OfficerCard({ teamMember }: OfficerCardProps) {
   let iconColor: string = "";
 
   return (
     <div className="mt-4">
       <div className="w-24 h-24 mx-auto">
-        <img src={item.avatar} className="w-full h-full rounded-full" alt="" />
+        <img src={teamMember.avatar} className="w-full h-full rounded-full" alt="" />
       </div>
       <div className="mt-2">
-        <h4 className="font-bold sm:text-lg text-primary-focus">{item.name}</h4>
-        <p className="font-semibold">{item.title}</p>
-        <p className="mt-2 px-2">{item.desc}</p>
+        <h4 className="font-bold sm:text-lg text-primary-focus">{teamMember.name}</h4>
+        <p className="font-semibold">{teamMember.title}</p>
+        <p className="mt-2 px-2">{teamMember.desc}</p>
         <div className="mt-4 flex justify-center gap-4 text-gray-400">
-          <a href={item.linkedin}>
+          <a href={teamMember.linkedin}>
             <LinkedIn color={iconColor} />
           </a>
-          <a href={item.github}>
+          <a href={teamMember.github}>
             <GitHub color={iconColor} />
           </a>
-          <a href={item.email}>
+          <a href={teamMember.email}>
             <Email color={iconColor} />
           </a>
         </div>
