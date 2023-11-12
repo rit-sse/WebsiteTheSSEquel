f859bcff916f0330f9e60138a484f810cf307941
Ryan <39078738+2nPlusOne@users.noreply.github.com>
2023-11-12 00:39:31 -0500

    Modified CSS to add dynamic spacing around p tags

--- next/app/about/primary-officers-policy/page.tsx
+++ next/app/about/primary-officers-policy/page.tsx
@@ -3,9 +3,15 @@ import { CTAButton } from '@/components/common/CTAButton';
 import { getPostData } from "@/lib/posts";
 
 export default async function PrimaryOfficersPolicy() {
-    const postData = await getPostData("https://raw.githubusercontent.com/rit-sse/governing-docs/main/primary-officers-policy.md");
+  const postData = await getPostData("https://raw.githubusercontent.com/rit-sse/governing-docs/main/primary-officers-policy.md");
 
   return (
-      <div dangerouslySetInnerHTML={{ __html: postData.props.htmlContent }} />
-  );        
+    <section>
+      <div className="flex flex-col items-center">
+        <div className="mx-auto px-4 sm: py-16 md:pb-8 lg:max-w-6xl">
+          <div dangerouslySetInnerHTML={{ __html: postData.props.htmlContent }} />
+        </div>
+      </div>
+    </section>
+  );
 }
