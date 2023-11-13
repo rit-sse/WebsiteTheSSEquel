804402f18b1950d5d7ca8af95eb75373b3305da2
Ryan <39078738+2nPlusOne@users.noreply.github.com>
2023-11-13 13:33:21 -0500

    fixed icon alignment in OfficerCard.ts

--- next/app/about/leadership/OfficerCard.tsx
+++ next/app/about/leadership/OfficerCard.tsx
@@ -15,7 +15,7 @@ export default function OfficerCard({ teamMember }: OfficerCardProps) {
         <h4 className="font-bold sm:text-lg text-primary-focus">{teamMember.name}</h4>
         <p className="font-semibold">{teamMember.title}</p>
         <p className="mt-2 px-2 text-center">{teamMember.desc}</p>
-        <div className="mt-4 flex gap-4">
+        <div className="flex flex-row gap-4 mt-4 justify-center items-center">
           <a href={teamMember.linkedin}>
             <LinkedInIcon />
           </a>
