91d2c007058f809a4f771efc8c6ba893425985e6
Ashton Michelstein <aam1727@g.rit.edu>
2023-11-18 16:38:23 -0500

    page.tsx in test folder

---
+++ next/app/go/test/page.tsx
@@ -0,0 +1,23 @@
+// import GoLink from ".././GoLink";
+
+const goLinkData: { goUrl: string; url: string; description: string }[] = [
+    {
+        goUrl: "scoreboard",
+        url: "https://sse.rit.edu/go/scoreboard",
+        description: "The new membership scoreboard created with google sheets!",
+    },
+    {
+        goUrl: "movienight",
+        url: "https://sse.rit.edu/go/movienight",
+        description: "Movie Night",
+    },
+    {
+        goUrl: "challenges-leaderboard",
+        url: "https://sse.rit.edu/go/challenges-leaderboard",
+        description: "The leaderboard for the leaderboard for the weekly coding challenges",
+    },
+];
+
+export default function PinnedGoLinks() {
+    return(<h1>Pinned</h1>)
+}
