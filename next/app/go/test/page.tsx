cf928874b91c132add357fd6d558a548b8158f51
Ashton Michelstein <aam1727@g.rit.edu>
2023-11-18 15:18:17 -0500

    2 columns

--- next/app/go/test/page.tsx
+++ next/app/go/test/page.tsx
@@ -64,9 +64,9 @@ export default function GoLinks() {
             <div className="
                 grid
                 grid-cols-1
-                sm:grid-cols-2
-                md:grid-cols-3
-                lg:grid-cols-4
+                sm:grid-cols-1
+                md:grid-cols-2
+                lg:grid-cols-2
                 gap-4
                 p-4
             ">
