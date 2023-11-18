dc94f5799ca5fded1c4e44b2a6a15a45b2119957
PokeJofeJr4th <PokeJofeJr4th@gmail.com>
2023-11-18 15:35:42 -0500

    add Course API routes

---
+++ next/app/api/course/[id]/route.ts
@@ -0,0 +1,41 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+/**
+ * HTTP GET request to /api/course/[id]
+ * @param request
+ * @param param1 { params: { id: string } }
+ * @returns course with { id }
+ */
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  // make sure the provided ID is a valid integer
+  try {
+    const id = parseInt(params.id);
+    const course = await prisma.course.findUnique({
+      where: {
+        id,
+      },
+      select: {
+        title: true,
+        code: true,
+        department: {
+          select: {
+            title: true,
+            shortTitle: true,
+          },
+        },
+      },
+    });
+    // make sure the selected course exists
+    if (course == null) {
+      return new Response(`Didn't find Course ID ${id}`, { status: 404 });
+    }
+    return Response.json(course);
+  } catch {
+    return new Response("Invalid Course ID", { status: 422 });
+  }
+}
