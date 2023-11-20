7a9f9cb4008ec267c21b0ebe1594d3fe5699d963
ntr0pie <tripathi.shreeshv@gmail.com>
2023-11-20 00:43:56 -0500

    Add card columns

--- next/app/projects/ProjectCard.tsx
+++ next/app/projects/ProjectCard.tsx
@@ -2,7 +2,15 @@ import React from "react";
 
 const ProjectCard = () => {
   return (
+    <div className="rounded-lg bg-base-100 w-9/12 py-8 px-12 mx-auto flex flex-row items-center content-center gap-10 my-10">
+      {/* Left half */}
+      <div className="text-9xl">🐞</div>
 
+      {/* Right half */}
+      <div>
+        
+      </div>
+    </div>
   );
 };
 
