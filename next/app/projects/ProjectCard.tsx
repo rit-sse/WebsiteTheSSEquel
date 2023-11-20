e3c07c544508a87185798a6aebf22319f69cf907
ntr0pie <tripathi.shreeshv@gmail.com>
2023-11-20 00:44:25 -0500

    Add fields and placeholder text

--- next/app/projects/ProjectCard.tsx
+++ next/app/projects/ProjectCard.tsx
@@ -9,6 +9,40 @@ const ProjectCard = () => {
       {/* Right half */}
       <div>
         
+        {/* Heading */}
+        <h1 className="text-2xl text-primary text-left mb-4">
+          Project Name 
+        </h1>
+        
+        {/* Body */}
+        <div className="text-lg flex flex-col gap-2">
+          <div>
+            {/* <div className="mb-2"> */}
+            <span className="font-bold">Supervisor: </span>
+            Name
+          </div>
+
+          <div>
+            {/* <div className="mb-2"> */}
+            <span className="font-bold">Contact: </span>
+            personal@email.com
+          </div>
+
+          <div>
+            {/* <div className="mb-2"> */}
+            <span className="font-bold">Description: </span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia,
+            aliquid. Laudantium veritatis veniam praesentium nesciunt facilis
+            quod quam nihil labore perferendis, qui molestiae ad quibusdam
+            magnam consequatur tempore, hic minus!
+          </div>
+
+          <div>
+            {/* <div className="mb-2"> */}
+            <span className="font-bold">Tech Stack: </span>
+            HTML/CSS, JavaScript, React, Next
+          </div>
+        </div>
       </div>
     </div>
   );
