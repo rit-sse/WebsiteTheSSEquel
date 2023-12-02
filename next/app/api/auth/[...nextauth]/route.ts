79ffbb344e19e7bfbd83d2072f0b9a733ef6e7c8
saltyypanda <esh7943@rit.edu>
2023-12-02 17:20:52 -0500

    Moved authOptions component to its own file, changed import paths

--- next/app/api/auth/[...nextauth]/route.ts
+++ next/app/api/auth/[...nextauth]/route.ts
@@ -1,31 +1,6 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
+import { authOptions } from "@/lib/authOptions";
 import NextAuth, { AuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
 
-const prisma = new PrismaClient()
-
-export const authOptions: AuthOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID || "",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-            authorization: {
-                params: {
-                    hd: "g.rit.edu" // restrict logins to rit.edu accounts
-                }
-            }
-        })
-    ],
-    callbacks: {
-        session: async ({ session, user }) => {
-            // fetch user roles from database
-            // session.roles = ...
-            return Promise.resolve(session)
-        }
-    }
-};
 
 export const handler = NextAuth(authOptions)
 
