6bd1b0acf2c56bb6c60d13f50d3c20143e163e69
Ashton Michelstein <aam1727@g.rit.edu>
2023-12-06 15:23:33 -0500

    numbers on bottom of carousel

--- next/app/go/PinnedGoLinksContainer.tsx
+++ next/app/go/PinnedGoLinksContainer.tsx
@@ -3,15 +3,22 @@ import PinnedGoLink from "./PinnedGoLink";
 
 const PinnedGoLinksContainer: React.FC<GoLinksContainerProps> = ({goLinkData}) => {
         const goLinkList = goLinkData.slice(0,7).map((data, index) => (
-               <div className="carousel-item" key={index}>
+               <div className="carousel-item" key={index} id={`item-${index}`}>
                 <PinnedGoLink {...data}/>
                 </div>
             ));
         return (
                 <div>
-                  <div className = "carousel carousel-end max-w-screen-md mx-auto" >
+                  <div className = "carousel carousel-center max-w-screen-md mx-auto" >
                         {goLinkList}
                   </div> 
+                  <div className="flex justify-center w-full py-2 gap-2">
+                {goLinkData.slice(0, 7).map((_, index) => (
+                    <a href={`#item-${index}`} className="btn btn-xs" key={index}>
+                        {index + 1}
+                    </a>
+                ))}
+            </div>
                 </div>
         )
         
