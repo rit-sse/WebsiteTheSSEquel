09c2e6377b5014d05abb80c0e7569add99b9fe49
Christian <cmmorgado53@gmail.com>
2023-12-06 16:27:55 -0500

    cleaned up pinned go link component

--- next/app/go/PinnedGoLink.tsx
+++ next/app/go/PinnedGoLink.tsx
@@ -1,3 +1,5 @@
+import { GoLinkStar } from "@/components/common/Icons";
+
 export default function PinnedGoLink({goUrl, url, description} : {goUrl: string, url: string, description: string}) {
     return (
         <a
@@ -13,15 +15,23 @@ export default function PinnedGoLink({goUrl, url, description} : {goUrl: string,
             transition-shadow
             border-2
             border-base-content
-            hover:border-info"
+            hover:border-info
+            mx-2"
             >
-                <div className = "flex-grow overflow-auto whitespace-normal w-96">
-                    <p className = "font-bold text-2xl">
+                <div className="flex items-center">
+                    {/* {<GoLinkStar/>} */}
+                    <p className="font-bold text-2xl">
                         {goUrl}
                     </p>
-                    <p className = "text-base">{description}</p>
-
                 </div>
+                {/* <div className = "flex-grow overflow-auto whitespace-normal w-96">
+                    <p className = "font-bold text-2xl">
+                        <GoLinkStar />
+                        {goUrl}
+                    </p>
+                    { <p className = "text-base">{description}</p> }
+
+                </div> */}
 
 
             </a>
