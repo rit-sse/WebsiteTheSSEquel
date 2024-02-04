3eb3b0e8a5706e791056675ac779c78432481833
Garrett59100 <garrett59100@gmail.com>
2024-02-04 16:01:04 -0500

    Created POST Request

--- next/app/api/mentorSkill/route.ts
+++ next/app/api/mentorSkill/route.ts
@@ -5,6 +5,7 @@ const prisma = new PrismaClient();
 export async function GET() {
 	const mentorSkill = await prisma.mentorSkill.findMany({
 		select: {
+			id: true,
 			mentor_Id: true,
 			skill_Id: true,
 			mentor: {
@@ -24,4 +25,27 @@ export async function GET() {
 		},
 	});
 	return Response.json(mentorSkill);
+}
+
+export async function POST(request: Request) {
+	let body;
+	try {
+		body = await request.json();
+	} catch {
+		return new Response("Invalid JSON", { status: 422 });
+	}
+
+	if (!("mentor_Id" in body && "skill_Id" in body)) {
+		return new Response('"mentor_Id" and "skill_Id" must be included in request body', {
+			status: 400,
+		});
+	}
+
+	const mentorSkill = await prisma.mentorSkill.create({
+		data: {
+			mentor_Id: body.mentor_Id,
+			skill_Id: body.skill_Id,
+		},
+	});
+	return Response.json(mentorSkill, { status: 201 });
 }
