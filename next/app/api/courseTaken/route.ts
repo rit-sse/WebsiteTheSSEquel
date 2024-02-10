2e3d88b0890d3b10aaffbcfe39a1466943e397b5
Garrett59100 <garrett59100@gmail.com>
2024-02-10 16:32:05 -0500

    Created Post Request + Documentation

--- next/app/api/courseTaken/route.ts
+++ next/app/api/courseTaken/route.ts
@@ -2,6 +2,10 @@ import { Prisma, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+/**
+ * HTTP GET request to /api/courseTaken
+ * @returns list of courseTaken objects
+ */
 export async function GET() {
 	const coursesTaken = await prisma.courseTaken.findMany({
 		select: {
@@ -28,4 +32,38 @@ export async function GET() {
 	});
 
 	return Response.json(coursesTaken);
+}
+
+/**
+ * HTTP POST request to /api/courseTaken
+ * @param {number} request.mentorId identifier for mentor
+ * @param {number} request.courseId identifier for course
+ * @returns takenCourse object that was created
+ */
+export async function POST(request: Request) {
+	let body;
+	try {
+		body = await request.json();
+	} catch {
+		return new Response("Invalid JSON", { status: 422 });
+	}
+
+	if (!("mentorId" in body && "courseId" in body)) {
+		return new Response("mentorId and courseId must be in body", { status: 422 });
+	}
+
+	console.log(body.mentorId);
+	console.log(body.courseId);
+
+	try {
+		const takenCourse = await prisma.courseTaken.create({
+			data: {
+				mentorId: body.mentorId,
+				courseId: body.courseId,
+			},
+		});
+		return Response.json(takenCourse, { status: 201 });
+	} catch (e) {
+		return new Response(`Failed to create takenCourse: ${e}`, { status: 500 });
+	}
 }
