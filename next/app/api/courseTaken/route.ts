888d8b7f6471eebad2a416e70d8de2eb5b9e4256
Garrett59100 <garrett59100@gmail.com>
2024-02-10 16:45:14 -0500

    Created PUT Request

--- next/app/api/courseTaken/route.ts
+++ next/app/api/courseTaken/route.ts
@@ -36,6 +36,7 @@ export async function GET() {
 
 /**
  * HTTP POST request to /api/courseTaken
+ * @param {Object} request body of the HTTP POST request
  * @param {number} request.mentorId identifier for mentor
  * @param {number} request.courseId identifier for course
  * @returns takenCourse object that was created
@@ -52,9 +53,6 @@ export async function POST(request: Request) {
 		return new Response("mentorId and courseId must be in body", { status: 422 });
 	}
 
-	console.log(body.mentorId);
-	console.log(body.courseId);
-
 	try {
 		const takenCourse = await prisma.courseTaken.create({
 			data: {
@@ -65,5 +63,41 @@ export async function POST(request: Request) {
 		return Response.json(takenCourse, { status: 201 });
 	} catch (e) {
 		return new Response(`Failed to create takenCourse: ${e}`, { status: 500 });
+	}
+}
+
+/**
+ * HTTP PUT request to /api/courseTaken
+ * @param {Object} request body of the HTTP PUT request
+ * @param {number} request.id id of the object being updated
+ * @param {number|undefined} request.mentorId identifier for mentor
+ * @param {number|undefined} request.courseId identifier for course
+ * @returns takenCourse object that was updated
+ */
+export async function PUT(request: Request) {
+	let body;
+	try {
+		body = await request.json();
+	} catch {
+		return new Response("Invalid JSON", { status: 422 });
+	}
+
+	if (!("id" in body)) {
+		return new Response("id must be in body", { status: 422 });
+	}
+
+	try {
+		const takenCourse = await prisma.courseTaken.update({
+			where: {
+				id: body.id,
+			},
+			data: {
+				mentorId: body.mentorId,
+				courseId: body.courseId,
+			},
+		});
+		return Response.json(takenCourse);
+	} catch (e) {
+		return new Response(`Failed to update courseTaken: ${e}`, { status: 500 });
 	}
 }
