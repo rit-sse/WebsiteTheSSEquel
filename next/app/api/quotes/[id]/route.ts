cb9242603c9337c5cdf49a8b515293caf6ff5530
Samuel Velasquez <SamuelVelasquez@protonmail.com>
2024-02-17 14:51:40 -0500

    fixed indentation and altered put function of quote api

--- next/app/api/quotes/[id]/route.ts
+++ next/app/api/quotes/[id]/route.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 /**
  * HTTP GET request to api/quotes/[id]
@@ -8,33 +8,36 @@ const prisma = new PrismaClient()
  * @param param1 { params: { id: string } }
  * @returns quote with { id }
  */
-export async function GET(request: Request, { params }: { params: { id: string } }) {
-    try {
-        const id = parseInt(params.id);
-        const quote = await prisma.quote.findUnique({
-            where: {
-                id,
-            },
-            select: {
-                date_added: true,
-                quote: true,
-                user_id: true,
-                user: {
-                    // testing purposes only, may remove later
-                    select: {
-                        firstName: true,
-                        lastName: true,
-                        email: true
-                    }
-                },
-                author: true
-            }
-        });
-        if (id == null) {
-            return new Response(`Could not find Quote ID ${id}`, { status: 404 });
-        }
-        return Response.json(quote);
-    } catch {
-        return new Response("Invalid Quote ID", { status: 422 });
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const id = parseInt(params.id);
+    const quote = await prisma.quote.findUnique({
+      where: {
+        id,
+      },
+      select: {
+        date_added: true,
+        quote: true,
+        user_id: true,
+        user: {
+          // testing purposes only, may remove later
+          select: {
+            firstName: true,
+            lastName: true,
+            email: true,
+          },
+        },
+        author: true,
+      },
+    });
+    if (id == null) {
+      return new Response(`Could not find Quote ID ${id}`, { status: 404 });
     }
+    return Response.json(quote);
+  } catch {
+    return new Response("Invalid Quote ID", { status: 422 });
+  }
 }
--- next/app/api/quotes/route.ts
+++ next/app/api/quotes/route.ts
@@ -1,29 +1,29 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 /**
  * HTTP GET request to api/quotes
  * @returns list of quote objects in model
  */
 export async function GET() {
-    const quotes = await prisma.quote.findMany({
+  const quotes = await prisma.quote.findMany({
+    select: {
+      date_added: true,
+      quote: true,
+      user_id: true,
+      user: {
+        // testing purposes only, may remove later
         select: {
-            date_added: true,
-            quote: true,
-            user_id: true,
-            user: {
-                // testing purposes only, may remove later
-                select: {
-                    firstName: true,
-                    lastName: true,
-                    email: true
-                }
-            },
-            author: true
-        }
-    })
-    return Response.json(quotes)
+          firstName: true,
+          lastName: true,
+          email: true,
+        },
+      },
+      author: true,
+    },
+  });
+  return Response.json(quotes);
 }
 
 /**
@@ -32,92 +32,109 @@ export async function GET() {
  * @returns quote object that was created
  */
 
-// TODO: Test
 export async function POST(request: Request) {
-    const body = await request.json()
+  let body;
+  try {
+    body = await request.json();
+  } catch {
+    return new Response("Invalid JSON", { status: 422 });
+  }
 
-    if (!("dateAdded" in body && "quote" in body && "userId" in body)) {
-        return new Response(
-            '"userId", "dateAdded", "quote", must be included in request body',
-            { status: 400 }
-        );
-    }
+  //validate request body
+  if (!("dateAdded" in body && "quote" in body && "userId" in body)) {
+    return new Response(
+      '"userId", "dateAdded", "quote", must be included in request body',
+      { status: 400 }
+    );
+  }
 
-    const date_added = new Date(body.dateAdded)
-    const quote = body.quote
-    const user_id = body.userId
+  const date_added = new Date(body.dateAdded);
+  const quote = body.quote;
+  const user_id = body.userId;
 
-    // fill in author key if one was specified by user. Otherwise, leave it anonymous
-    let author
-    if (body.author) {
-        author = body.author
-    }
+  // fill in author key if one was specified by user. Otherwise, leave it anonymous
+  let author;
+  if ("author" in body) {
+    author = body.author;
+  }
 
+  try {
     const create_quote = await prisma.quote.create({
-        data: {
-            date_added,
-            quote,
-            user_id,
-            author
-        }
+      data: {
+        date_added,
+        quote,
+        user_id,
+        author,
+      },
     });
-
-    return Response.json(create_quote, { status: 201 })
+    return Response.json(create_quote, { status: 201 });
+  } catch (e) {
+    return new Response(`Failed to create quote: ${e}`, { status: 500 });
+  }
 }
 
 /**
  * PUT request to /api/quote
- * @param request { dateAdded: Date, quote: string, user_id: number, author?: string }
+ * @param request { dateAdded?: Date, quote?: string, userId?: number, author?: string }
  * @returns updated quote object
  */
 export async function PUT(request: Request) {
-    let body;
-    try {
-        body = await request.json();
-    } catch {
-        return new Response("Invalid JSON", { status: 422 });
-    }
+  let body;
+  try {
+    body = await request.json();
+  } catch {
+    return new Response("Invalid JSON", { status: 422 });
+  }
 
-    //check if id is in request
-    if (!("id" in body || "dateAdded" in body || "quote" in body || "user_id" in body)) {
-        return new Response("`id`, `dateAdded`, `quote`, and `user_id` must be included in request body", { status: 400 })
-    }
-    const id = body.id;
-    
-    //check if user_id is valid
-    try {
-        prisma.user.findUniqueOrThrow({
-            where: { id: body.user_id}
-        });
-    } catch {
-        return new Response("invalid user_id value", { status: 404 })
-    }
+  //check if id is in request
+  if (!("id" in body)) {
+    return new Response('"id" must be included in request body', {
+      status: 400,
+    });
+  }
+  const id = body.id;
 
-    const data: {
-        dateAdded: Date;
-        quote: string;
-        user_id: number;
-        author?: string;
-    } = {
-        dateAdded: new Date(body.dateAdded),
-        quote: body.quote,
-        user_id: body.userId
-    };
-    
-    if ("author" in body) {
-        data.author = body.user_id
-    }
+  //check if user_id is valid
+  try {
+    prisma.user.findUniqueOrThrow({
+      where: { id: body.userId },
+    });
+  } catch {
+    return new Response("invalid userId value", { status: 404 });
+  }
 
-    try {
-        const quote = await prisma.quote.update({
-            where: { id },
-            data,
-        });
-        return Response.json(quote);
-    } catch (e) {
-        return new Response(`Failed to update quote: ${e}`, { status: 500 });
-    }
+  const data: {
+    date_added?: Date;
+    quote?: string;
+    user_id?: number;
+    author?: string;
+  } = {};
 
+  if ("dateAdded" in body) {
+    data.date_added = new Date(body.dateAdded);
+  }
+
+  if ("quote" in body) {
+    data.quote = body.quote;
+  }
+
+  if ("userId" in body) {
+    data.user_id = body.userId;
+  }
+
+  if ("author" in body) {
+    data.author = body.author;
+  }
+
+  try {
+    const quote = await prisma.quote.update({
+      where: { id },
+      data,
+    });
+    return Response.json(quote);
+  } catch (e) {
+    return new Response(`Failed to update quote: ${e}`, { status: 500 });
+  }
 }
 
 /**
@@ -128,18 +145,18 @@ export async function PUT(request: Request) {
 
 //TODO: Test
 export async function DELETE(request: Request) {
-    const body = await request.json()
+  const body = await request.json();
 
-    if (!("id" in body)) {
-        return new Response("id of quote must be included", {status: 400})
-    }
+  if (!("id" in body)) {
+    return new Response("id of quote must be included", { status: 400 });
+  }
 
-    const id = body.id
-    const quoteExists = prisma.quote.findUnique({where: {id}});
-    if (!quoteExists) {
-        return new Response("quote does not exist", {status: 404})
-    }
+  const id = body.id;
+  const quoteExists = prisma.quote.findUnique({ where: { id } });
+  if (!quoteExists) {
+    return new Response("quote does not exist", { status: 404 });
+  }
 
-    const quote = await prisma.quote.delete({where: { id }})
-    return Response.json(quote)
+  const quote = await prisma.quote.delete({ where: { id } });
+  return Response.json(quote);
 }
