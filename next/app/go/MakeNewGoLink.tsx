099008eea63eb3d2d548e2bfe2ef2bf1d9310e6f
Max Liu <maxliu@maxs-air-3.wireless.rit.edu>
2024-02-25 15:38:19 -0500

    IS GOOOD NOW

--- next/app/go/MakeNewGoLink.tsx
+++ next/app/go/MakeNewGoLink.tsx
@@ -4,18 +4,20 @@ import { GoLinkStar } from "@/components/common/Icons";
 export const GoLinkButton: React.FC = () =>  {
     return (
         <button className="
-        btn
+
         p-4
         h-full
         bg-base-100
         rounded-md
         shadow-md
+        justify-items-center
         hover:shadow-lg
         transition-shadow
         border-2
         border-base-content
         hover:border-info
-        ">Make New Go-Link</button>
+        text-xl
+        ">Make New Go Link </button>
     );
 }
 
