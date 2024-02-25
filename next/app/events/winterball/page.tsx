4eef0abbfe7af01f4da1bec6f4dc3d1732ee8d05
Ashton Michelstein <aam1727@g.rit.edu>
2024-02-25 15:20:48 -0500

    funky stuff

---
+++ next/app/events/winterball/page.tsx
@@ -0,0 +1,15 @@
+const WinterBall = () => {
+    return (
+        <div className="diff aspect-[16/9]">
+  <div className="diff-item-1">
+    <div className="bg-primary text-primary-content text-9xl font-black grid place-content-center">DAISY</div>
+  </div>
+  <div className="diff-item-2">
+    <div className="bg-base-200 text-9xl font-black grid place-content-center">DAISY</div>
+  </div>
+  <div className="diff-resizer"></div>
+</div>
+    )
+}
+
+export default WinterBall
