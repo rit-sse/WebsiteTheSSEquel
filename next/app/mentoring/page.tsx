627c702b4848e8dcb4ba6dade3ee5c0646130837
Hawker <62062830+BlckHawker@users.noreply.github.com>
2024-02-25 17:24:14 -0500

    Add time and days for calendar

--- next/app/mentoring/page.tsx
+++ next/app/mentoring/page.tsx
@@ -3,7 +3,21 @@ const MentorPage = () => {
     return (
         <div id='content'>
             <div id='mentor-info'>Metnor Info</div>
-            <div id='mentor-canlendar'>Mentor Calendar</div>
+            <div id='mentor-canlendar'>
+                <p id='monday'>Mon</p>
+                <p id='tuesday'>Tue</p>
+                <p id='wednesday'>Wed</p>
+                <p id='thursday'>Thu</p>
+                <p id='friday'>Fri</p>
+                <p id='ten'>10 - 11</p>
+                <p id='eleven'>11 - 12</p>
+                <p id='twelve'>12 - 1</p>
+                <p id='one'>1 - 2</p>
+                <p id='two'>2 - 3</p>
+                <p id='three'>3 - 4</p>
+                <p id='four'>4 - 5</p>
+                <p id='five'>5 - 6</p>
+            </div>
         </div>
     )
 }
