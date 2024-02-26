753b96215596fc327a0908c056b0fa8e69296ebc
Max Liu <maxliu@maxs-air-3.wireless.rit.edu>
2024-02-26 11:25:52 -0500

    modal shows up for go link create

--- next/app/go/MakeNewGoLink.tsx
+++ next/app/go/MakeNewGoLink.tsx
@@ -2,11 +2,17 @@ import { GoLinkIcon } from "@/components/common/Icons";
 import { GoLinkStar } from "@/components/common/Icons";
 import { useSession } from "next-auth/react";
 
-export const GoLinkButton: React.FC = (func) =>  {
+export const GoLinkButton: React.FC = () =>  {
     const { data: session } = useSession()
     if(session){
         return (
-            <button className="
+            <button 
+            onClick={(func) =>{
+                func.preventDefault();
+                document.getElementById('make_new_link').showModal();
+                }
+            }
+            className="
             p-4
             h-full
             bg-base-100
@@ -20,7 +26,17 @@ export const GoLinkButton: React.FC = (func) =>  {
             hover:border-info
             text-xl"
             >Make New Go Link
+            <dialog id="make_new_link" className="modal">
+                <div className="modal-box">
+                    <form method="dialog">
+                    <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                    </form>
+                    <h3 className="font-bold text-lg">Make New Go Link</h3>
+                    <p className="py-4">Press ESC key or click on ✕ button to close</p>
+                </div>
+            </dialog>
             </button>
+            
         );
     }
 }
