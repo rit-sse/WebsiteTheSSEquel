72a9565886edbc9b315401b1fb870be098aafcea
Ashton Michelstein <aam1727@g.rit.edu>
2024-03-02 23:41:57 -0500

    color change

--- next/app/events/winterball/page.tsx
+++ next/app/events/winterball/page.tsx
@@ -1,9 +1,10 @@
+
 const WinterBall = () => {
   
     return (
-      <div className="diff lg:h-72 lg:aspect-[16/9] sm:aspect-[16/9]">
+      <div id = "diff" className="diff lg:h-72 lg:aspect-[16/9] sm:aspect-[16/9]">
         <div className="diff-item-1">
-          <div className="bg-primary text-primary-content font-black grid place-content-center text-4xl sm:text-9xl">Winter Ball</div>
+          <div className="bg-primary font-black text-white grid place-content-center text-4xl sm:text-9xl">Winter Ball</div>
         </div>
         <div className="diff-item-2">
           <div className="bg-base-200 font-black grid place-content-center text-4xl sm:text-9xl">Spring Fling</div>
