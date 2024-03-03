f9c35a3fd3bcdbd3a891ea0b0cdb52a15a01d394
PokeJofeJr4th <PokeJofeJr4th@gmail.com>
2024-03-03 14:24:27 -0500

    golink redirect works with DB call and new API route!

--- next/lib/middlewares/golinks.ts
+++ next/lib/middlewares/golinks.ts
@@ -3,12 +3,20 @@ import { isUrlValid } from "../utils";
 // import goLinkData from "@/app/go/goLinkData";
 
 const getDestinationUrl = async (goUrl: string) => {
-    // for (let goLink of goLinkData) {
-    //     if (goLink.goUrl === goUrl) {
-    //         return goLink.url;
-    //     }
-    // }
-}
+  const response = await fetch(process.env.NEXTAUTH_URL + "/api/go/" + goUrl);
+  if (response.ok) {
+    const url = await response.text();
+    return url.startsWith("http") ? url : "https://" + url;
+  } else {
+    console.log("Error finding golink", await response.text());
+    return null;
+  }
+  // for (let goLink of goLinkData) {
+  //     if (goLink.goUrl === goUrl) {
+  //         return goLink.url;
+  //     }
+  // }
+};
 
 /** Middleware to handle golinks.
  * Checks the following:
@@ -16,28 +24,28 @@ const getDestinationUrl = async (goUrl: string) => {
  * - if the go link exists in the data store
  * - if the destination is a valid URL
  * - if the destination is a live site
- * 
+ *
  * If all checks pass, redirects to the destination.
  * Otherwise, returns NextResponse.next() to continue the middleware chain.
  */
 export const golinksMiddleware = async (request: NextRequest) => {
-    const { pathname } = request.nextUrl;
-    // Only run golinks middleware logic for paths starting with "/go/"
-    if (pathname.startsWith('/go/')) {
-        // const goLink = pathname.split('/go/')[1];
-        // const destination = await getDestinationUrl(goLink); // this would be replaced with a database lookup
+  const { pathname } = request.nextUrl;
+  // Only run golinks middleware logic for paths starting with "/go/"
+  if (pathname.startsWith("/go/")) {
+    const goLink = pathname.split("/go/")[1];
+    const destination = await getDestinationUrl(goLink); // this would be replaced with a database lookup
 
-        // // If the destination exists and is valid, redirect to it
-        // if (destination && isUrlValid(destination)) {
-        //     // check if the url is a live site
-        //     const response = await fetch(destination);
-        //     if (response.ok) {
-        //         // redirect to the destination
-        //         return NextResponse.redirect(destination);
-        //     }
-        // }
+    // If the destination exists and is valid, redirect to it
+    if (destination && isUrlValid(destination)) {
+      // check if the url is a live site
+      const response = await fetch(destination);
+      if (response.ok) {
+        // redirect to the destination
+        return NextResponse.redirect(destination);
+      }
     }
+  }
 
-    // Signal to continue the middleware chain (see middleware.ts)
-    return NextResponse.next();
-}
+  // Signal to continue the middleware chain (see middleware.ts)
+  return NextResponse.next();
+};
