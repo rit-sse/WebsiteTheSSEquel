b2ebe29703cad702fd103533fcc0e4ed4a75b9a1
Christian <cmmorgado53@gmail.com>
2024-03-03 14:12:58 -0500

    links to be fixed

--- next/lib/middlewares/golinks.ts
+++ next/lib/middlewares/golinks.ts
@@ -1,6 +1,25 @@
 import { NextRequest, NextResponse } from "next/server";
 import { isUrlValid } from "../utils";
+import { useCallback, useEffect, useState } from "react";
 // import goLinkData from "@/app/go/goLinkData";
+
+// const [goLinkData, setGoLinkData] = useState([]);
+// const fetchData = useCallback(async() => {
+//     const response = await fetch("http://localhost:3000/api/golinks/public");
+//     const data = await response.json();
+//     setGoLinkData(data.map((item: { id: number, golink: string; url: string; description: string; isPinned: boolean; }) => ({
+//         id: item.id,
+//         goUrl: item.golink,
+//         url: item.url,
+//         description: item.description ?? '', 
+//         pinned: item.isPinned, 
+//     })));
+// }, [])
+// useEffect(() => {
+//     fetchData()
+// }, [fetchData]);
+
+// useEffect(() => {}, [goLinkData]); 
 
 const getDestinationUrl = async (goUrl: string) => {
     // for (let goLink of goLinkData) {
@@ -21,11 +40,12 @@ const getDestinationUrl = async (goUrl: string) => {
  * Otherwise, returns NextResponse.next() to continue the middleware chain.
  */
 export const golinksMiddleware = async (request: NextRequest) => {
+    console.log("Request NextURL: " + request.nextUrl);
     const { pathname } = request.nextUrl;
     // Only run golinks middleware logic for paths starting with "/go/"
     if (pathname.startsWith('/go/')) {
-        // const goLink = pathname.split('/go/')[1];
-        // const destination = await getDestinationUrl(goLink); // this would be replaced with a database lookup
+        const goLink = pathname.split('/go/')[1];
+        const destination = await getDestinationUrl(goLink); // this would be replaced with a database lookup
 
         // // If the destination exists and is valid, redirect to it
         // if (destination && isUrlValid(destination)) {
