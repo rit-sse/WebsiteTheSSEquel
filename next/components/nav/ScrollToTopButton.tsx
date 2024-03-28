b8d74341776fb9facc75af0096dd13639c2217fd
zl8244 <zl8244@g.rit.edu>
2024-03-28 15:51:06 -0400

    scroll button color now fits with theme

--- next/components/nav/ScrollToTopButton.tsx
+++ next/components/nav/ScrollToTopButton.tsx
@@ -8,7 +8,7 @@ const ScrollToTopButton = () => {
   useEffect(() => {
     const toggleVisibility = () => {
       // if the user scrolls down, show the button
-      window.scrollY > 200 ? setIsVisible(true) : setIsVisible(false)
+      window.scrollY > 400 ? setIsVisible(true) : setIsVisible(false)
     }
     // listen for scroll events
     window.addEventListener("scroll", toggleVisibility)
@@ -30,7 +30,7 @@ const ScrollToTopButton = () => {
 
   return (
     <button
-      className={`fixed bottom-12 right-4 p-2 bg-primary rounded-full font-medium transition-opacity duration-200 ${
+      className={`fixed bottom-16 right-4 p-2 bg-primary rounded-full font-medium transition-opacity duration-200 ${
         isVisible ? "opacity-100" : "opacity-0"
       }`}
       onClick={scrollToTop}
@@ -41,7 +41,7 @@ const ScrollToTopButton = () => {
         fill="currentColor"
         stroke="currentColor"
         className="w-6 h-6">
-      <path d="M18.2929 15.2893C18.6834 14.8988 18.6834 14.2656 18.2929 13.8751L13.4007 8.98766C12.6195 8.20726 11.3537 8.20757 10.5729 8.98835L5.68257 13.8787C5.29205 14.2692 5.29205 14.9024 5.68257 15.2929C6.0731 15.6835 6.70626 15.6835 7.09679 15.2929L11.2824 11.1073C11.673 10.7168 12.3061 10.7168 12.6966 11.1073L16.8787 15.2893C17.2692 15.6798 17.9024 15.6798 18.2929 15.2893Z" fill="#0F0F0F"/>
+      <path d="M18.2929 15.2893C18.6834 14.8988 18.6834 14.2656 18.2929 13.8751L13.4007 8.98766C12.6195 8.20726 11.3537 8.20757 10.5729 8.98835L5.68257 13.8787C5.29205 14.2692 5.29205 14.9024 5.68257 15.2929C6.0731 15.6835 6.70626 15.6835 7.09679 15.2929L11.2824 11.1073C11.673 10.7168 12.3061 10.7168 12.6966 11.1073L16.8787 15.2893C17.2692 15.6798 17.9024 15.6798 18.2929 15.2893Z"/>
       </svg>
     </button>
   )
