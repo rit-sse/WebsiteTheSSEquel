84c0bb81ba4754ee235085f074849f7f404129b6
Christian <cmmorgado53@gmail.com>
2024-04-15 11:44:25 -0400

    fixed svg error

--- next/components/nav/ScrollToTopButton.tsx
+++ next/components/nav/ScrollToTopButton.tsx
@@ -46,8 +46,8 @@ const ScrollToTopButton = () => {
         fill="currentColor"
         stroke="currentColor"
         className="h-5 w-5"
-        fill-rule="evenodd" 
-        clip-rule="evenodd">
+        fillRule="evenodd" 
+        clipRule="evenodd">
         <path d="M12 7C12.2652 7 12.5196 7.10536 12.7071 7.29289L19.7071 14.2929C20.0976 14.6834 20.0976 15.3166 19.7071 15.7071C19.3166 16.0976 18.6834 16.0976 18.2929 15.7071L12 9.41421L5.70711 15.7071C5.31658 16.0976 4.68342 16.0976 4.29289 15.7071C3.90237 15.3166 3.90237 14.6834 4.29289 14.2929L11.2929 7.29289C11.4804 7.10536 11.7348 7 12 7Z"/>
       </svg>
     </button>
