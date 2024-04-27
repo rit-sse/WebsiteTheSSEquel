933690a65e9aac4a9354c8162532e3677765298a
Christian <cmmorgado53@gmail.com>
2024-04-27 12:08:12 -0400

    Im gonna fart

---
+++ next/app/events/springfling/UpcomingEvents.tsx
@@ -0,0 +1,12 @@
+"use client"
+import { useState, useEffect } from 'react';
+
+const UpcomingEvents = () => {
+   return (
+        <>
+
+        </>
+   );
+}
+
+export default UpcomingEvents;
---
+++ next/app/events/springfling/page.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from "next";
+import UpcomingEvents from "./UpcomingEvents";
+
+export const metadata: Metadata = {
+    title: "Events Calendar",
+    description:
+      "The Society of Software Engineers hosts a wide variety of weekly events, ranging from talks and company visits to committee meetings and large seasonal parties."
+};
+
+export default function EventsCalendar() {
+    return (
+        <>
+            <div className="flex flex-col items-center w-full h-screen max-w-screen-xl">            
+                <UpcomingEvents/>
+            </div>
+        </>
+    );
+}
