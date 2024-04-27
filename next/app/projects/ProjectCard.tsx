212a0c16d98b34386687c57b87ef689c67731c23
saltyypanda <esh7943@rit.edu>
2024-04-27 12:42:37 -0400

    Made it so all images take up the same width

--- next/app/projects/ProjectCard.tsx
+++ next/app/projects/ProjectCard.tsx
@@ -6,12 +6,15 @@ const ProjectCard = ({logo, title, lead, contact, description, stack, progress}:
   return (
     <div className="rounded-lg bg-base-100 w-10/12 py-8 px-12 mx-auto flex flex-row items-center content-center gap-10 my-10">
       {/* Left half */}
-      <Image 
-        src={logo} 
-        alt={title}
-        width="400"
-        height="240"
-      />
+      <div className="flex justify-center items-center object-cover w-[400px] h-[240px] overflow-hidden">
+        <Image 
+          src={logo}
+          alt={title}
+          width={400}
+          height={240}
+          objectFit="cover"
+        />
+      </div>
 
       {/* Right half */}                                   
       <div>
