9648a985aa2427002d57dcce81729cdd3c7f797b
Jacob Miller <68158267+jcm10x1@users.noreply.github.com>
2024-09-22 13:21:46 -0400

    Fixed mobile login

--- next/components/common/HoverBoldButton.tsx
+++ next/components/common/HoverBoldButton.tsx
@@ -12,7 +12,7 @@ export default function HoverBoldButton({ text, dataLabel, onClick, className }:
             onClick={() => onClick()}
             data-label={dataLabel}
             aria-label={text}
-            className={`flex-grow bold-pseudo cursor-pointer px-3 py-2
+            className={`flex-grow  bold-pseudo cursor-pointer px-3 py-2
                         hover:text-primary focus:text-primary
                         hover:font-semibold transition-all ${className}`}
         >
