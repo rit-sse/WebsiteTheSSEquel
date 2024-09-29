f1b9e1546f360d0bd2110f74438b7ca22dd266c9
AriasLcr <ariaslcr@gmail.com>
2024-09-29 13:24:26 -0400

    EventCards responsive
    EventCards can now morph into landscape.

--- next/app/events/EventCard.tsx
+++ next/app/events/EventCard.tsx
@@ -3,8 +3,10 @@ import { Event } from "./event";
 
 export const EventCard: React.FC<Event> = (event: Event) => {
     return (
-        <div className="flex flex-col shadow-md rounded-3xl overflow-hidden bg-info-content max-w-[340px]">
-            <img src={event.imageSrc} className="w-full" alt="" />
+        <div className="flex flex-row xl:flex-col shadow-md rounded-3xl overflow-hidden bg-info-content max-w-[90%] xl:max-w-[340px]  h-[300px] xl:h-[600px] m-4">
+        {/* These are the previous class attributes, they are staying here for convenience. */}
+        {/* <div className="flex flex-col shadow-md rounded-3xl overflow-hidden bg-info-content max-w-[340px]  h-[600px] md:h-[600px] m-4"> */}
+            <img src={event.imageSrc} className="object-cover w-[45%]  xl:w-full" alt="" />
             <div className="p-4">
                 <h4 className="text-primary">{event.title.slice(0, 1).toUpperCase() + event.title.slice(1)}</h4> {/* Make sure the title is captialized, but otherwise, this is just the title of the card */}
                 <h5>{event.date}</h5>
--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,7 +1,6 @@
 // This file renders the home page route (/) of the website.
 // We know that this is the homepage because this file resides in the root of the `app` directory.
 
-import Image from 'next/image'
 import { CTAButton } from '@/components/common/CTAButton';
 import HomepageContent from './HomepageContent';
 import { UpcomingEvents } from './HomepageContent';
@@ -30,21 +29,19 @@ export default function Home() {
                         <CTAButton href="/about/get-involved" text="Get Involved" />
                     </div>
                 </div>
-                <div className='flex mt-12 md:mt-0 w-11/12 sm:w-[45%] sm:h-full justify-center'>
+                <div className='flex mt-12 md:mt-0 w-11/12 md:w-[45%] sm:h-full justify-center'>
                     <img src="student-involvement-1.jpg" alt="Tech committee meeting" className="w-full h-auto rounded-[60px]" />
                 </div>
             </div>
 
-            {/* Upcoming Ev ents */}
+            {/* Upcoming Events */}
             <div>
               <h1 className='mt-5'>Upcoming Events</h1>
-              <div className='flex lg:flex-row sm:flex-col justify-center items-center'>
-                <div className='mt-8 grid md:gap-8 lg:gap-4 grid-cols-3 w-10/12'>
+                <div className='flex flex-col xl:flex-row justify-center items-center mt-8 md:gap-8 lg:gap-4 '>
                     {UpcomingEvents.map((event, idx) => (
                         <EventCard key={idx} {...event} />
                     ))}
                 </div>
-              </div>
             </div>
         </div>
     );
