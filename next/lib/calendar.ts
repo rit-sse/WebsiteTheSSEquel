6e08b4ff9f11a54fbb7874da9c074e4df587dc0a
PokeJofeJr4th <PokeJofeJr4th@gmail.com>
2024-10-06 14:49:40 -0400

    set up the calendar lib file

---
+++ next/lib/calendar.ts
@@ -0,0 +1,71 @@
+import jwt from "jsonwebtoken";
+import fs from "fs";
+
+const config = JSON.parse(fs.readFileSync("config.json", "utf8"));
+
+let accessToken = new Promise<string>((resolve, reject) => {
+  resolve("");
+});
+let expiry = 0;
+
+export const getToken = async () => {
+  if (expiry > Math.floor(Date.now() / 1000) + 3300) {
+    return await accessToken;
+  }
+
+  accessToken = new Promise<string>(async (resolve, reject) => {
+    const token = jwt.sign(
+      {
+        iss: config.client_email,
+        scope:
+          "https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.events",
+        aud: "https://oauth2.googleapis.com/token",
+        exp: Math.floor(Date.now() / 1000) + 3600,
+        iat: Math.floor(Date.now() / 1000),
+      },
+      config.private_key,
+      {
+        algorithm: "RS256",
+      }
+    );
+
+    const res = await fetch("https://oauth2.googleapis.com/token", {
+      method: "POST",
+      body: JSON.stringify({
+        grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
+        assertion: token,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = (await res.json()) as any;
+    accessToken = data.access_token;
+    expiry = Math.floor(Date.now() / 1000) + data.expires_in;
+    resolve(accessToken);
+  });
+
+  // set the expiry so that anyone else calling this function awaits our promise
+  expiry = Math.floor(Date.now() / 1000) + 3600;
+  return await accessToken;
+};
+
+// const fetchCalendar = async () => {
+//   // mintime is the current time in RFC3339 format
+//   const mintime = new Date().toISOString();
+
+//   const calendarRes = await fetch(
+//     "https://www.googleapis.com/calendar/v3/calendars/????/events?timeMin=" +
+//       mintime,
+//     {
+//       method: "GET",
+//       headers: {
+//         Authorization: `Bearer ${accessToken}`,
+//       },
+//     }
+//   );
+
+//   const calendarData = (await calendarRes.json()) as any;
+//   console.log(calendarData);
+// };
