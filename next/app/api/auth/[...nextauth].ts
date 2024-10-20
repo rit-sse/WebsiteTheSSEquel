512fc6aad19516d2c4920ef26517ef8dab8d7596
PokeJofeJr4th <PokeJofeJr4th@gmail.com>
2024-10-20 13:47:47 -0400

    replace auth/prisma-adapter with next-auth/prisma-adapter

--- next/app/api/auth/[...nextauth].ts
+++ next/app/api/auth/[...nextauth].ts
@@ -1,23 +1,23 @@
-import prisma from '@/lib/prisma';
-import { PrismaAdapter } from '@auth/prisma-adapter';
-import { PrismaClient } from '@prisma/client';
-import NextAuth, { AuthOptions } from 'next-auth';
-import Google from 'next-auth/providers/google';
-import GoogleProvider from 'next-auth/providers/google';
+import prisma from "@/lib/prisma";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import NextAuth, { AuthOptions } from "next-auth";
+import Google from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: AuthOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID || '',
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-            authorization: {
-                params: {
-                    hd: 'g.rit.edu', // restrict logins to rit.edu accounts
-                },
-            },
-        }),
-    ],
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+      authorization: {
+        params: {
+          hd: "g.rit.edu", // restrict logins to rit.edu accounts
+        },
+      },
+    }),
+  ],
 };
 
 export default NextAuth(authOptions);
