d6d6fbc3c5ea7a23df7d5f195d38499d98d4617c
AriasLcr <ariaslcr@gmail.com>
2024-11-03 16:55:30 -0500

    DaisyUI Hero Component Refactor

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -9,29 +9,29 @@ import Image from 'next/image';
 
 export default function Home() {
     return (
-        <div className='space-y-24'>
-            {/* Hero section */}
-            <div className='h-auto md:h-[55vh] my-auto flex flex-col md:flex-row items-center md:justify-evenly mt-24'>
-                <div className="flex flex-col justify-center w-auto md:w-2/5">
-                    <h1 className='text-center md:text-left'>
-                        Society of
-                        <span className="block lg:inline"> Software Engineers </span>
-                    </h1>
-
-                    <p className="mx-auto mt-4 sm:text-xl/relaxed text-center md:text-left">
-                        {HomepageContent.description}
-                    </p>
-                    <p className='mt-4 font-bold text-center md:text-left'>
-                        {HomepageContent.weeklyMeetingCallout}
-                    </p>
-
-                    <div className="mt-8 flex flex-row flex-wrap gap-4 min-[460px]:flex-row justify-center md:justify-start">
-                        <CTAButton href={HomepageContent.slackLink} text="Join Slack" />
-                        <CTAButton href="/about/get-involved" text="Get Involved" />
+        <div className='space-y-24 mt-10'>
+            {/* Hero Component */}
+            <div className="hero h-[80vh]">
+                <div className="hero-content flex-col lg:flex-row">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold leading-relaxed text-center lg:text-left">
+                        Society of 
+                            <span className="block lg:inline"> Software Engineers </span>
+                        </h1>
+                        <p className="py-6">
+                            {HomepageContent.description}
+                        </p>
+                        <p className="mt-4 font-bold">
+                            {HomepageContent.weeklyMeetingCallout}
+                        </p>
+                        <div className="mt-8 flex flex-wrap gap-4 justify-center lg:justify-start">
+                            <CTAButton href={HomepageContent.slackLink} text="Join Slack" />
+                            <CTAButton href="/about/get-involved" text="Get Involved" />
+                        </div>
                     </div>
-                </div>
-                <div className='flex mt-12 md:mt-0 w-11/12 md:w-[45%] sm:h-full justify-center'>
-                    <Image src={`/student-involvement-1.jpg`} alt="Tech committee meeting" className="w-full h-auto rounded-[60px]" width={1000} height={1000} />
+                    <div className="mt-12 lg:mt-0">
+                        <Image src="/student-involvement-1.jpg" alt="Tech committee meeting" className="rounded-[60px]" width={1000} height={1000} priority />
+                    </div>
                 </div>
             </div>
 
