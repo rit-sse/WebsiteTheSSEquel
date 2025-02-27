2f3b1587a61ece9a449d23ec136a15e4b8343b98
MaxLiuLilum <maxliulilum@gmail.com>
2025-02-27 13:28:56 -0500

    linking my github accound

--- next/app/mentoring/mentor.ts
+++ next/app/mentoring/mentor.ts
@@ -10,7 +10,7 @@ export interface Mentors{
     courses: string[]
     color: string
 }
-
+//max Liulilum
 export const mockmentors: Mentors[] = [
     {name: "John Mentor", time: [AllMentorTime[2][2],AllMentorTime[3][1],AllMentorTime[1][2]],courses:["SWEN-261","SWEN-123","SWEN-444"],color:'red'},
     {name: "SSE Dave", time: [AllMentorTime[1][2],AllMentorTime[3][4],AllMentorTime[2][3]],courses:["SWEN-261","SWEN-214","SWEN-250"],color:'blue'},
