a28f882a7fcd3047c54b2b36b49e75a77b841eb3
Jonathan Hesler <jeh8443@rit.edu>
2025-03-22 16:41:59 -0400

    Adding id's to placeholder quotes

--- next/app/qdb/Quotes.ts
+++ next/app/qdb/Quotes.ts
@@ -1,4 +1,5 @@
 export interface Quote {
+    id: number,
     quote: string,
     description: string,
     tags: string[],
@@ -6,11 +7,13 @@ export interface Quote {
 
 export const Quotes: Quote[] = [
     {
+        id: 1,
         quote: "[Gary] I'm in ya mother's DM's\n[Sylvia] So you're using an Ouija Board?",
         description: ":skull:",
         tags: ["Gary", "Sylvia"]
     },
     {
+        id: 2,
         quote: "[Eric] I'm allergic to hoes, bros, and nonbinary foes",
         description: "Better being an Epi-Pen! Lab is full of 'em",
         tags: ["Eric"]
