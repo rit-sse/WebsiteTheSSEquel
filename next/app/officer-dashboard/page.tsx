acea37d719fdf906c82bcc51c2caae9217f2cd39
tres <59184373+pybash@users.noreply.github.com>
2025-09-10 13:48:25 -0400

    Officer Dashboard Setup

---
+++ next/app/officer-dashboard/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React, { Dispatch, SetStateAction, useEffect } from "react";
+
+const OfficerDashboard = () => {
+  return (
+    <>
+    <h2>Officer Dashboard</h2>
+    </>
+  );
+};
+
+export default OfficerDashboard;
