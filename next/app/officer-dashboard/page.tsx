7be52e904b9d70f99f5be36233947e096ec3d99e
tres <59184373+pybash@users.noreply.github.com>
2025-09-12 10:59:12 -0400

    Setup

--- next/app/officer-dashboard/page.tsx
+++ next/app/officer-dashboard/page.tsx
@@ -1,10 +1,17 @@
 "use client";
-import React, { Dispatch, SetStateAction, useEffect } from "react";
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 
 const OfficerDashboard = () => {
   return (
     <>
-    <h2>Officer Dashboard</h2>
+      <div className="w-[80%] flex items-start justify-between">
+        <div className="w-[24%] h-[150px] bg-white rounded-[14px]">
+          <p className="w-full pl-[20px] py-[10px] border-b-[1px]">Settings</p>
+        </div>
+        <div className="w-[75%] h-[150px] bg-white rounded-[14px]">
+
+        </div>
+      </div>
     </>
   );
 };
