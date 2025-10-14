b9de8d7a04cde40244dcc9bae830d6ca1294c331
Cayden Tisdale <caydentisdale43@gmail.com>
2025-10-13 21:47:03 -0400

    small api fix

--- next/app/api/memberships/route.ts
+++ next/app/api/memberships/route.ts
@@ -14,7 +14,9 @@ export async function GET() {
         },
 
         orderBy: {
-            userId: 'desc'
+            _count: {
+                userId: 'desc'
+            }
         }
     });
 
