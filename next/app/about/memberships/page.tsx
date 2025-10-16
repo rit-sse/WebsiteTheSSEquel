f22ec47e55825c9e9243a3d169316a6d46b41fee
Cayden Tisdale <caydentisdale43@gmail.com>
2025-10-16 01:39:45 -0400

    Update page with add membership button

--- next/app/about/memberships/page.tsx
+++ next/app/about/memberships/page.tsx
@@ -3,22 +3,36 @@
 import { useEffect, useState } from "react";
 import { LeaderboardItem } from "./item";
 import { LeaderboardTable } from "./LeaderboardTable";
+import { AddMembershipModal } from "./AddMembershipModal";
 
 export default function Leaderboard() {
     const [items, setItems] = useState<LeaderboardItem[]>([]);
+    const [open, setOpen] = useState(false);
 
-    useEffect(() => {
-        (async () => {
+    async function load() {
         const res = await fetch("/api/memberships");
         const data = await res.json();
         setItems(data);
-        })().catch(console.error);
-    }, []);
+    }
+
+    useEffect(() => {
+        load();
+    }, [])
 
     return (
         <div className="p-6">
+            <div className="flex items-center justify-between gap-2">
+                <h1 className="text-xl font-semibold">Memberships Leaderboard</h1>
+                <button className="btn btn-success" onClick={() => setOpen(true)}>Add Membership</button>
+            </div>
+
             <LeaderboardTable items={items}/>
+            <AddMembershipModal open={open} onOpenChange={setOpen} onCreated={load}></AddMembershipModal>
         </div>
+        
+        
+    
+        
     )
 
 }
