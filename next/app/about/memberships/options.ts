dca166126ecd8c9a49e63e8bb8eddd8ac701b43b
Cayden Tisdale <caydentisdale43@gmail.com>
2025-10-16 01:39:23 -0400

    AddMembershipModal and interfaces

---
+++ next/app/about/memberships/options.ts
@@ -0,0 +1,11 @@
+export interface AutocompleteOption {
+    id: number
+    name: string
+    email: string
+}
+
+export interface Membership {
+    userId: number;
+    reason: string;
+    dateGiven: string;
+}
