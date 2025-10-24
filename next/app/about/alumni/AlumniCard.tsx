0f7f097aaf750dafe4d463cc4e2ae6717c599485
tres <59184373+pybash@users.noreply.github.com>
2025-10-24 16:16:21 -0400

    Positioning and Image to img

--- next/app/about/alumni/AlumniCard.tsx
+++ next/app/about/alumni/AlumniCard.tsx
@@ -12,13 +12,13 @@ export default function AlumniCard({ alumniMember }: AlumniCardProps) {
     <div className="mt-4 w-full flex justify-center">
       <div className="mt-2 flex flex-col items-center w-full max-w-xs sm:max-w-sm px-4">
         {alumniMember.image != "https://source.boringavatars.com/beam/" ? (
-          <Image src={alumniMember.image} alt="Photo of team member" width={96} height={96} className="rounded-full object-cover"/> 
+          <img src={alumniMember.image} alt="Photo of team member" className="rounded-full object-cover h-[96px] w-[96px]"/> 
         ) : (
           <Avatar size={96} name={alumniMember.name || "default"} colors={["#426E8C", "#5289AF", "#86ACC7"]} variant="beam"/>
         )}
         <h4 className="font-bold sm:text-lg text-primary-focus text-center">{alumniMember.name}</h4>
-        <p className="font-semibold text-center">&quot;{alumniMember.quote}&quot;</p>
-        <p className="font-semibold text-center">{alumniMember.previous_roles}&nbsp;</p>
+        <p className="font-semibold text-center">"{alumniMember.quote}"</p>
+        <p className="font-semibold text-center">{alumniMember.previous_roles}</p>
         <p className="mt-2 px-2 text-center font-semibold">{alumniMember.end_date}</p>
 
         <div className="w-full flex flex-row gap-4 justify-center items-center">
