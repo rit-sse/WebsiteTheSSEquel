94a21a81d4103ecfd484b0af3d886c12ae7da53b
xaizko <tonytran177013@gmail.com>
2025-11-24 20:26:01 -0500

    switched to prisma client

--- next/app/api/scheduleBlock/route.ts
+++ next/app/api/scheduleBlock/route.ts
@@ -1,7 +1,9 @@
 import { NextRequest, NextResponse } from "next/server"
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "@/lib/authOptions"
-import { prisma } from "@/lib/prisma"
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export async function POST(request: NextRequest) {
     try {
