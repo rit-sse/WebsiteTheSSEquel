cdd16ccfeaeb5853618676be96e1c7356468858a
J0m1ty <jomity@hotmail.com>
2025-11-30 18:12:17 -0500

    Add data driven theming support

--- next/tailwind.config.ts
+++ next/tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from 'tailwindcss'
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 const config: Config = {
+  darkMode: ['class', '[data-theme="dark"]'],
   daisyui: {
     themes: [
       {
